6ddd42dbe84901d28aef03af532ca023dfb625e7
AnatoliyShevchenko <genitalgrinder90@outlook.com>
2025-07-03 21:19:25 +0500

    temp

--- settings/urls.py
+++ settings/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from debug_toolbar.toolbar import debug_toolbar_urls
 from rest_framework.routers import DefaultRouter
 from rest_framework import permissions
