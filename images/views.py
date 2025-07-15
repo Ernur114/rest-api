81f3cda5fa4d4ee8418c6f7646d4f4c413018050
Ernur <ernur200820@gmail.com>
2025-07-15 18:32:52 +0500

    add

--- images/views.py
+++ images/views.py
@@ -1,19 +1,19 @@
-# from rest_framework.viewsets import ModelViewSet
-# from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 
-# from images.models import Gallery, Images, Avatar
-# from images.serializers import (
-#     GallerySerializer, ImagesSerializer, AvatarSerializer
-# )
-
-
-# class GalleryView(ModelViewSet):
-#     queryset = Gallery.objects.all()
-#     permission_classes = [AllowAny]
-#     serializer_class = GallerySerializer
+from images.models import Gallery, Images, Avatar
+from images.serializers import (
+    GallerySerializer, ImagesSerializer, AvatarSerializer
+)
 
 
-# class ImagesView(ModelViewSet):
-#     queryset = Images.objects.all()
-#     permission_classes = [AllowAny]
-#     serializer_class = ImagesSerializer
+class GalleryView(ModelViewSet):
+    queryset = Gallery.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = GallerySerializer
+
+
+class ImagesView(ModelViewSet):
+    queryset = Images.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = ImagesSerializer
